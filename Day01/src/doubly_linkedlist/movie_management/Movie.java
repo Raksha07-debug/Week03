581b8b2b5b225025f4fe8fbf14bef4cf0a3871f3
Raksha <rak070803@gmail.com>
2025-02-03 11:00:06 +0530

    Completed Movie Management

---
+++ Day01/src/doubly_linkedlist/movie_management/Movie.java
@@ -0,0 +1,20 @@
+package doubly_linkedlist.movie_management;
+
+
+class Movie {
+    String title;
+    String director;
+    int year;
+    double rating;
+    Movie next;
+    Movie prev;
+
+    public Movie(String title, String director, int year, double rating) {
+        this.title = title;
+        this.director = director;
+        this.year = year;
+        this.rating = rating;
+        this.next = null;
+        this.prev = null;
+    }
+}
