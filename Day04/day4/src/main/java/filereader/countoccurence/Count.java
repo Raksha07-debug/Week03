90415a3dace06e0f3286dec41abdadde7e1f4375
Raksha <rak070803@gmail.com>
2025-02-05 16:38:59 +0530

    [Raksha] Refactor. Count occurence

--- Day04/day4/src/main/java/filereader/countoccurence/Count.java
+++ Day04/day4/src/main/java/filereader/countoccurence/Count.java
@@ -1,29 +1,36 @@
 package filereader.countoccurence;
 
+
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.*;
 
-public class Count {
-    public static void main(String[] args) {
-        String path="C:\\Week03\\Day04\\day4\\src\\main\\java\\filereader\\countoccurence\\countfile.txt";
-        int count=0;
-        String targetWord="miracle";
-        try(BufferedReader br= new BufferedReader(new FileReader(path))){
-            String line;
-            while((line=br.readLine())!=null){
-                String  words[]=line.split("\\s+");
+public class Count{
+    public static int wordOccurence(String filePath, String target){
+        int count = 0;
+
+
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+            String line = "";
+            while((line = br.readLine())!=null){
+                String[] words = line.split("\\s+");
                 for(String word:words){
-                    if(word.equalsIgnoreCase(targetWord)){
+                    if(word.equalsIgnoreCase(target)){
                         count++;
                     }
                 }
             }
         }
-        catch(IOException e){
+        catch (IOException e){
+            System.out.println("Error occured"+e.getMessage());
             e.printStackTrace();
         }
-        System.out.println("The word '" + targetWord + "' appears " + count + " times in the file.");    }
+        return  count;
+    }
+
+    public static void main(String[] args) {
+        String s = "C:\\Gabgemini\\Week03\\String-Manipulation\\src\\main\\java\\filereader\\couuntoccurence\\countword.txt";
+        String target = "wood";
+        System.out.println("Count:"+wordOccurence(s,target));
+    }
 }
