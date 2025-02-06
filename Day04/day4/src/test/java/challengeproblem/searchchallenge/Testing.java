ed004b2bf4cff04c9bbcc0202f49b47034d2e958
Raksha <rak070803@gmail.com>
2025-02-06 12:07:22 +0530

    [Raksha] Added. Challenge problem linear search v/s binary search

---
+++ Day04/day4/src/test/java/challengeproblem/searchchallenge/Testing.java
@@ -0,0 +1,22 @@
+package challengeproblem.searchchallenge;
+
+import challengeproblem.binearsearchvslinearsearch.SearchChallenge;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Testing {
+    @Test
+    void testFindFirstMissingPositive(){
+        int[] array = {3, 4, -1, 1, 9, 2};
+        int output = 5;
+        Assertions.assertEquals(output, SearchChallenge.findFirstMissingPositive(array));
+    }
+
+    @Test
+    void testBinarySearch(){
+        int[] array = {3, 4, -1, 1, 9, 2};
+        int target = 9;
+        int outputIndex = 4;
+        Assertions.assertEquals(outputIndex, SearchChallenge.binarySearch(array, target));
+    }
+}
