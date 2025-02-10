04602871057b6d8f99a2b1bf10d36a069320d4b1
Raksha <rak070803@gmail.com>
2025-02-10 10:36:28 +0530

    [Raksha] Added: large file reading efficiency

---
+++ Day06/algorithm/src/main/java/com/largefilereadingefficiency/LargeFileReadingEfficiency.java
@@ -0,0 +1,45 @@
+package com.largefilereadingefficiency;
+
+
+
+import java.io.*;
+public class LargeFileReadingEfficiency {
+    public static void main(String[] args) {
+        compares();
+    }
+    public static int[] compares(){
+        int arr[]=new int[2];
+        String filePath = "C:\\Week03\\Day06\\file1.txt";
+        // Using FileReader
+        try {
+            long startTime = System.currentTimeMillis();
+            FileReader fileReader = new FileReader(filePath);
+            int ch;
+            while ((ch = fileReader.read()) != -1) {
+                // Process character (here, we're just reading)
+            }
+            fileReader.close();
+            long endTime = System.currentTimeMillis();
+            arr[0]= (int) (endTime - startTime);
+            System.out.println("Time taken using FileReader: " + (endTime - startTime) + " ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Using InputStreamReader
+        try {
+            long startTime = System.currentTimeMillis();
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(filePath));
+            int ch;
+            while ((ch = inputStreamReader.read()) != -1) {
+                // Process character (here, we're just reading)
+            }
+            inputStreamReader.close();
+            long endTime = System.currentTimeMillis();
+            arr[1]= (int) (endTime - startTime);
+            System.out.println("Time taken using InputStreamReader: " + (endTime - startTime) + " ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return arr;
+    }
+}
